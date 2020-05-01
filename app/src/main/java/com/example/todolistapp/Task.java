86e4cb5003a3a52b1b88667246632bef367d10b8
mikhail <cocain1988@gmail.com>
2020-05-01 20:33:17 +0300

    First commit after create project.

---
+++ app/src/main/java/com/example/todolistapp/Task.java
@@ -0,0 +1,60 @@
+package com.example.todolistapp;
+
+import java.util.Date;
+
+public class Task {
+    private String TaskName;
+    private boolean Status;
+    private Date RemindDate;
+    private Date RunDate;
+    private boolean Repeat;
+    private String GlobalDescription;
+
+    public String getTaskName() {
+        return TaskName;
+    }
+
+    public void setTaskName(String taskName) {
+        TaskName = taskName;
+    }
+
+    public boolean isStatus() {
+        return Status;
+    }
+
+    public void setStatus(boolean status) {
+        Status = status;
+    }
+
+    public Date getRemindDate() {
+        return RemindDate;
+    }
+
+    public void setRemindDate(Date remindDate) {
+        RemindDate = remindDate;
+    }
+
+    public Date getRunDate() {
+        return RunDate;
+    }
+
+    public void setRunDate(Date runDate) {
+        RunDate = runDate;
+    }
+
+    public boolean isRepeat() {
+        return Repeat;
+    }
+
+    public void setRepeat(boolean repeat) {
+        Repeat = repeat;
+    }
+
+    public String getGlobalDescription() {
+        return GlobalDescription;
+    }
+
+    public void setGlobalDescription(String globalDescription) {
+        GlobalDescription = globalDescription;
+    }
+}
